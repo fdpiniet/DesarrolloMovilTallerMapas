b0773bc8a0dd674f7fd155b07390f634a90e79b9
Fabian Pineda <fdpiniet@gmail.com>
2016-09-25 16:57:54 -0500

    Mapa creado.

--- app/src/main/java/xyz/fabianpineda/desarrollomovil/tallermapas/MainActivity.java
+++ app/src/main/java/xyz/fabianpineda/desarrollomovil/tallermapas/MainActivity.java
@@ -2,12 +2,95 @@ package xyz.fabianpineda.desarrollomovil.tallermapas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap googleMap;
+    private SupportMapFragment mapFragment;
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        LatLng ubicacionUTB = new LatLng(10.370337, -75.465449);
+
+        this.googleMap.addMarker(new MarkerOptions().position(ubicacionUTB)).setVisible(true);
+
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacionUTB, 18));
+        this.googleMap.animateCamera(CameraUpdateFactory.zoomTo(18), 1, null);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_gps:
+
+                return true;
+            case R.id.menu_http:
+
+                return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_mapa, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapFragment = SupportMapFragment.newInstance();
+        getSupportFragmentManager().beginTransaction().add(R.id.activity_main, mapFragment).commit();
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (isFinishing()) {
+            // ...
+        } else {
+            // ...
+        }
     }
 }
